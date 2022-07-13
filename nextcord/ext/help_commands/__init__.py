9d2219a08e52849016081472c5cf00d1983f1fe7
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2022-07-13 15:48:13 +0200

    fix: make EmbeddedHelpCommand directly importable

--- nextcord/ext/help_commands/__init__.py
+++ nextcord/ext/help_commands/__init__.py
@@ -1,10 +1,8 @@
+from .embedded import EmbeddedHelpCommand
 from .errors import MissingDependencyError
 from .paginated import PaginatedHelpCommand
 
 # Needed for the setup.py script
 __version__ = "0.0.1"
 
-__all__ = (
-    "MissingDependencyError",
-    "PaginatedHelpCommand",
-)
+__all__ = ("MissingDependencyError", "PaginatedHelpCommand", "EmbeddedHelpCommand")
