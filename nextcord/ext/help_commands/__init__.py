0f1396f879d2b25561b65e59afbfc3d45d7cd70e
Jonah Lawrence <jonah@freshidea.com>
2022-07-14 18:48:31 -0600

    feat: basic slash help command

--- nextcord/ext/help_commands/__init__.py
+++ nextcord/ext/help_commands/__init__.py
@@ -1,10 +1,13 @@
 from .errors import MissingDependencyError
 from .paginated import PaginatedHelpCommand
+from .slash import SlashHelpCommand, MinimalSlashHelpCommand
 
 # Needed for the setup.py script
 __version__ = "0.0.1"
 
 __all__ = (
+    "MinimalSlashHelpCommand",
     "MissingDependencyError",
     "PaginatedHelpCommand",
+    "SlashHelpCommand",
 )
