966b4e99f54110267a5c52f88098ebdc4d974c6b
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2022-07-22 18:46:02 +0200

    style: put each item in __all__ on separate line

--- nextcord/ext/help_commands/__init__.py
+++ nextcord/ext/help_commands/__init__.py
@@ -5,4 +5,8 @@ from .paginated import PaginatedHelpCommand
 # Needed for the setup.py script
 __version__ = "0.0.1"
 
-__all__ = ("MissingDependencyError", "PaginatedHelpCommand", "EmbeddedHelpCommand")
+__all__ = (
+    "MissingDependencyError",
+    "PaginatedHelpCommand",
+    "EmbeddedHelpCommand",
+)
